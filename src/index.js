dffeb438d1d52e78eea12ba724ba2013a339e205
andrey.romanenkov <amicus765@gmail.com>
2017-10-06 14:31:36 +0300

    TryingUpdateElements2

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { render } from 'react-dom'
 
 import './css/style.css';
 
-//Некоторый начальный набор книг
+//Некоторый начальный набор книг в библиотеке
 var books = [
   {
     author: 'Замятин',
@@ -23,53 +23,7 @@ var books = [
   }
 ];
 
-//формирование списка книг - по одной
-var Article = React.createClass({
-  propTypes: {
-    data: React.PropTypes.shape({
-      author: React.PropTypes.string.isRequired,
-      book: React.PropTypes.string.isRequired,
-      year: React.PropTypes.string.isRequired
-    }) 
-  },
-
-  onBtnDelClickHandler: function(e) {
-    e.preventDefault();
-    this.props.onDelete(this.props.index);
-  },
-
-  onBtnUpdClickHandler: function(e){
-    e.preventDefault();
-    this.props.onUpdate(this.state.author, this.state.book,this.state.year,this.props.index);
-  },
-
-  render: function() {
-    var author = this.props.data.author,
-        book = this.props.data.book,
-        year = this.props.data.year;
-   
-    return (
-      <div className='article'>
-        <p className='book__author'>{author}:</p>
-        <p className='book__text'>{book}</p>
-        <p className='book__year' >{year} </p>
-       
-        <button className='del__btn'
-          onClick={this.onBtnDelClickHandler}
-          ref='del_button'
-          >
-         Удалить книгу </button>
-
-         <button className='update__btn'
-          onClick={this.onBtnUpdClickHandler}
-          ref='update_button'
-          >
-         Обновить книгу </button>
-      </div>
-    )
-  }
-});
-
+// библиотека книг
 const Library = React.createClass({
   propTypes: {
     data: React.PropTypes.array.isRequired
@@ -78,13 +32,13 @@ const Library = React.createClass({
   render: function() {
     var data = this.props.data;
     var libraryTemplate;
-    var self=this;
+    var that=this;
 
     if (data.length > 0) {
       libraryTemplate = data.map(function(item, index) {
         return (
-          <div key={index} index={index}>
-            <Article data={item} onDelete={self.props.onDelete} onUpdate={self.props.onUpdate}/>
+          <div key={index}>
+            <Article data={item} onDelete={that.props.onDelete} onSave={that.props.onSave} index={index}/>
           </div>
         )
       })
@@ -100,7 +54,49 @@ const Library = React.createClass({
   }
 });
 
-//добавление книг
+//формирование списка книг - по одной
+var Article = React.createClass({
+  propTypes: {
+    data: React.PropTypes.shape({
+      author: React.PropTypes.string.isRequired,
+      book: React.PropTypes.string.isRequired,
+      year: React.PropTypes.string.isRequired
+    }) 
+  },
+
+  onBtnDelClickHandler: function(e) {
+    e.preventDefault();
+    this.props.onDelete(this.props.index);
+  },
+
+  onElemUpdClickHandler: function(e){
+    e.preventDefault();
+    this.props.onSave(this.props.index);
+  },
+
+  render: function() {
+    var author = this.props.data.author,
+        book = this.props.data.book,
+        year = this.props.data.year;
+   
+    return (
+      <div className='article' onClick={this.onElemUpdClickHandler}>
+        <p className='book__author'>{author}:</p>
+        <p className='book__text'>{book}</p>
+        <p className='book__year' >{year} </p>
+       
+        <button className='del__btn'
+          onClick={this.onBtnDelClickHandler}
+          ref='del_button'
+          >
+         Удалить книгу </button>
+
+      </div>
+    )
+  }
+});
+
+//добавление и редактирование книг
 var Add = React.createClass({
   getInitialState: function() {
     return ({ //add constructor 
@@ -109,32 +105,38 @@ var Add = React.createClass({
       bookIsEmpty: true,
       yearIsEmpty: true,
 
-      author:this.props.author,
-      book: this.props.book,
-      year:this.props.year,
-
-      onBtnAddClickHandler: this.onBtnAddClickHandler.bind(this)
+      author:'',
+      book: '',
+      year:''
     });
   },
 
+  componentWillReceiveProps: function(nextProps) {
+     if(nextProps.articleEdit)
+      this.setState({
+        author:nextProps.articleEdit.author,
+        book:nextProps.articleEdit.book,
+        year:nextProps.articleEdit.year,
+        index:nextProps.articleEdit.index
+      });
 
-  //фокус на компоненте с именем thisInput при рендере
-  componentDidMount: function() {
-    this.nameInput.focus();
   },
-
 
   //обработчик добавления книги
   onBtnAddClickHandler: function(e) {
     e.preventDefault();
    
     //получить значения из input, передать их наверх родителю, родитель должен их отдать в Library
-    
     this.props.onAdd(this.state.author, this.state.book,this.state.year);
     
     this.setState({authorIsEmpty:true,bookIsEmpty:true,yearIsEmpty:true, author:'', book:'', year:''});
   },
   
+  onBtnUpdClickHandler: function(e){
+      e.preventDefault();
+      this.props.onUpdate(this.state.author, this.state.book,this.state.year,this.state.index);
+  },
+
   //отмечаем, что чекбокс включен/выключен
   onCheckRuleClick: function() {
     this.setState({agreeNotChecked: !this.state.agreeNotChecked});
@@ -178,21 +180,36 @@ var Add = React.createClass({
           className='add__year'
           id = 'year'
           value={this.state.year}
-          onChange={this.onChangeHandler}
-          placeholder='Год издания книги'
+          onChange = {this.onChangeHandler}
+          placeholder = 'Год издания книги'
         ></input>
 
         <label className='add__checkrule'>
           <input type='checkbox' onChange={this.onCheckRuleClick}/>Я согласен с правилами сайта
         </label>
 
+    {
+      !this.state.articleEdit && (
         <button
           className='add__btn'
-          onClick={this.onBtnAddClickHandler}
-          disabled={agreeNotChecked || authorIsEmpty || bookIsEmpty || yearIsEmpty}
+          onClick= {this.onBtnAddClickHandler}
+          disabled = {agreeNotChecked || authorIsEmpty || bookIsEmpty || yearIsEmpty}
           >
           Добавить книгу
-        </button>
+        </button>)
+    }
+
+    {
+      this.state.articleEdit && (
+        <button
+          className ='upd__btn'
+          onClick = {this.onBtnUpdClickHandler}
+          disabled = {agreeNotChecked || authorIsEmpty || bookIsEmpty || yearIsEmpty}
+          >
+          Редактировать книгу
+        </button>)
+    }
+
       </div>
     );
   }
@@ -216,29 +233,37 @@ const App = React.createClass({
  
   onDelete:function(delIndex)
   {
-    const new_library=[];
-    Object.assign(new_library,this.state.library);
-    new_library.splice(this.state.delIndex,1); 
-    this.setState({library:new_library});
+    const newLibrary=[];
+    Object.assign(newLibrary,this.state.library);
+    newLibrary.splice(delIndex,1); 
+    this.setState({library:newLibrary});
   },
 
-  onUpdate: function(author,book,year,updIndex)
-  {
-    const Book={author,book,year};
-    const new_library=[];
-    Object.assign(new_library,this.state.library);
-    new_library.splice(this.state.updIndex,1);//вырежем старый элемент
-    new.library.splice(this.state.updIndex, 0, Book);//добавим новый элемент
-    this.setState({library:new_library});
+  onSave: function(index){
+    this.setState({articleEdit:this.state.library[index]});
+  },
+
+  onUpdate: function(author,book,year,updIndex) {
+    const newLibrary= this.state.library.map(function(item) {
+      var tempItem = item;
+      if (tempItem.index===updIndex) {
+        tempItem.author = author;
+        tempItem.book = book;
+        tempItem.year = year;
+      }
+      return tempItem;
+    });
+
+    this.setState({library:newLibrary});
   },
 
   render: function() {
 
     return (
       <div className='app'>
-        <Add onAdd={this.onAdd}/>
+        <Add onAdd={this.onAdd} onUpdate={this.onUpdate} articleEdit={this.state.articleEdit}/>
         <h3>Библиотека</h3>
-        <Library data={this.state.library} onDelete={this.onDelete} onUpdate={this.onUpdate}/>
+        <Library data={this.state.library} onDelete={this.onDelete} onSave={this.onSave}/>
       </div>
     );
   }
